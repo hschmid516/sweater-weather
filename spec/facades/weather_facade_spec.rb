69fd391bf72e9b28e52ecd848701fe25c0710501
Henry Schmid <henry.schmid1@gmail.com>
2021-11-13 21:37:06 -0700

    Add weather facade test

---
+++ spec/facades/weather_facade_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe WeatherFacade, :vcr do
+  it 'gets forcast for city' do
+    forecast = WeatherFacade.forecast('Portland, ME')
+
+    expect(forecast).to be_a(Forecast)
+    expect(forecast.id).to be(nil)
+    expect(forecast.current_weather).to be_a(Hash)
+    expect(forecast.current_weather.size).to eq(10)
+    expect(forecast.daily_weather).to be_an(Array)
+    expect(forecast.daily_weather.size).to eq(5)
+    expect(forecast.daily_weather.first.size).to eq(7)
+    expect(forecast.hourly_weather).to be_an(Array)
+    expect(forecast.hourly_weather.size).to eq(8)
+    expect(forecast.hourly_weather.first.size).to eq(4)
+  end
+end
