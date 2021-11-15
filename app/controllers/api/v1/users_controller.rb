977da867912dfcfe53153c06dfa99147409425b0
Henry Schmid <henry.schmid1@gmail.com>
2021-11-15 16:52:09 -0700

    Update users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,19 @@
 class Api::V1::UsersController < ApplicationController
+  # def index
+  #   users = User.all
+  #   render json: UserSerializer.new(users)
+  # end
+
   def create
     user = User.create(user_params)
-    render json: UserSerializer.new(user), status: :created
+    if user.save
+      render json: UserSerializer.new(user), status: :created
+    else
+      bad_request(user)
+    end
   end
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_digest)
+    params.permit(:email, :password, :password_confirmation)
   end
 end
