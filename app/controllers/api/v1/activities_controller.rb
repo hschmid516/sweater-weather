089d95ca7c771940fe597e8a0baa5fb6510528d5
Henry Schmid <henry.schmid1@gmail.com>
2021-11-15 10:09:43 -0700

    Add to index action

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ActivitiesController < ApplicationController
   def index
-    
+    activities = ActivityFacade.activities(params[:destination])
+    render json: ActivitySerializer.new(activities)
   end
 end
