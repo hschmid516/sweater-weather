ec4df32841cd650aba5906983a6ff74282d85e9c
Henry Schmid <henry.schmid1@gmail.com>
2021-11-15 11:27:33 -0700

    Add serializer test

---
+++ spec/serializers/activities_serializer_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe ActivitiesSerializer, :vcr do
+  it 'serializes activities' do
+    activities = ActivityFacade.activities('chicago,il')
+    serialized = ActivitiesSerializer.new(activities).to_json
+
+    expect(serialized).to eq("{\"data\":{\"id\":null,\"type\":\"activities\",\"attributes\":{\"destination\":\"chicago,il\",\"forecast\":{\"summary\":null,\"temperature\":\"33.17 F\"},\"activities\":[{\"title\":\"Sit in the dark and listen to your favorite music with no distractions\",\"type\":\"relaxation\",\"participants\":1,\"price\":0},{\"title\":\"Bake a pie with some friends\",\"type\":\"cooking\",\"participants\":3,\"price\":0.3}]}}}")
+  end
+end
