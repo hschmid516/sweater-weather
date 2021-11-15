14dabfd14f0c6cf467012037b608b4c421727db7
Henry Schmid <henry.schmid1@gmail.com>
2021-11-15 09:36:29 -0700

    Update request test

--- spec/requests/v1/activities_request_spec.rb
+++ spec/requests/v1/activities_request_spec.rb
@@ -16,11 +16,16 @@ describe 'activities' do
     expect(attr[:forecast]).to be_a(Hash)
     expect(attr[:forecast][:summary]).to be_a(String)
     expect(attr[:forecast][:temperature]).to be_a(String)
-    expect(attr[:activities]).to be_a(Hash)
+    expect(attr[:activities]).to be_an(Array)
     expect(attr[:activities].length).to eq(2)
+
     attr[:activities].each do |activity|
-      activity
+      expect(activity[:title]).to be_a(String)
+      expect(activity[:type]).to be_a(String)
+      expect(activity[:participants]).to be_an(Integer)
+      expect(activity[:price]).to be_an(Float)
     end
+
     expect(attr).to_not have_key(:created_at)
     expect(attr).to_not have_key(:updated_at)
   end
