7f0149b519e907d61bd538613e613a4d1f26e65f
Henry Schmid <henry.schmid1@gmail.com>
2021-11-15 17:12:59 -0700

    Add sad path tests for login

--- spec/requests/v1/login_request_spec.rb
+++ spec/requests/v1/login_request_spec.rb
@@ -18,4 +18,27 @@ describe 'user login' do
     expect(json[:data][:attributes]).to_not have_key(:password_confirmation)
     expect(json[:data][:attributes]).to_not have_key(:password_digest)
   end
+
+  it 'has 404 error and message if bad password' do
+    user_params = {
+      email: user.email,
+      password: 'wrongpassword'
+    }
+
+    post '/api/v1/sessions', params: user_params
+
+    expect(response).to have_http_status(:unauthorized)
+    expect(json).to eq({ message: 'Credentials are missing or incorrect' })
+  end
+
+  it 'has 404 error and message if missing email' do
+    user_params = {
+      password: user.password
+    }
+
+    post '/api/v1/sessions', params: user_params
+
+    expect(response).to have_http_status(:unauthorized)
+    expect(json).to eq({ message: 'Credentials are missing or incorrect' })
+  end
 end
