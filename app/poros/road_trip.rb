a0abd1c6d3704e2b4e82669b136e4b5028259850
Henry Schmid <henry.schmid1@gmail.com>
2021-11-16 14:48:20 -0700

    Update initialize

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -2,10 +2,10 @@ class RoadTrip
   attr_reader :id, :start_city, :end_city, :travel_time, :weather_at_eta
 
   def initialize(start, end_city, travel_time, weather)
-    @start_city = start
-    @end_city = end_city
+    @start_city = start.gsub(/,/, ', ')
+    @end_city = end_city.gsub(/,/, ', ')
     @travel_time = Time.at(travel_time).utc.strftime "%H:%M"
-    @weather_at_eta = { temperature: weather.forecast.first[:temperature],
-                 conditions: weather.forecast.first[:conditions] }
+    @weather_at_eta = { temperature: weather.forecast[:temperature],
+                 conditions: weather.forecast[:conditions] }
   end
 end
