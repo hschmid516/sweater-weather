c4cbf52cebd74176c779d378d0d1fb38e8ea5010
Henry Schmid <henry.schmid1@gmail.com>
2021-11-16 14:48:37 -0700

    Add before_action to verify json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::API
   include ExceptionHandler
+  before_action :verify_json
 
   private
-  
+
   def verify_json
     bad_format if request.format != :json
   end
