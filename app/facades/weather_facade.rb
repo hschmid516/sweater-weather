fa326b827e36033f80c3cb6dfa47532a10525ff9
Henry Schmid <henry.schmid1@gmail.com>
2021-11-16 10:57:38 -0700

    Update weather facade

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,10 +1,10 @@
 class WeatherFacade
   class << self
-    def forecast(location)
-      location_info = WeatherService.find_lat_long(location)
+    def forecast(object, location, eta = nil)
+      location_info = MapService.find_lat_long(location)
       lat_long = location_info[:results].first[:locations].first[:latLng]
       forecast = WeatherService.forecast(lat_long)
-      Forecast.new(forecast)
+      object.new(forecast, eta)
     end
   end
 end
