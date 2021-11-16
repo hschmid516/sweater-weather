481832ef4326cc29ee1c723d4b2b902e6cf8691e
Henry Schmid <henry.schmid1@gmail.com>
2021-11-16 16:13:53 -0700

    Add helper methods for impossible routes

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -2,10 +2,19 @@ class RoadTripFacade
   class << self
     def plan_trip(start, end_city)
       travel_time = MapService.travel_time(start, end_city)
+      weather = WeatherFacade.forecast(TripForecast, end_city, eta(travel_time)) if travel_time
+      RoadTrip.new(start, end_city, verify_time(travel_time), weather)
+    end
+
+    private
+    
+    def verify_time(time)
+      time ? time : 'impossible route'
+    end
+
+    def eta(travel_time)
       eta = travel_time + Time.now.to_i
-      eta_rounded = (Time.at(eta) + 30.minutes).beginning_of_hour.to_i
-      weather = WeatherFacade.forecast(TripForecast, end_city, eta_rounded)
-      RoadTrip.new(start, end_city, travel_time, weather)
+      (Time.at(eta) + 30.minutes).beginning_of_hour.to_i
     end
   end
 end
