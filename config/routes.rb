cd1de8571baae6380b060145692db740fdea80c0
Henry Schmid <henry.schmid1@gmail.com>
2021-11-15 17:56:53 -0700

    Add json contstraints to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :forecast, only: :index
       resources :backgrounds, only: :index
-      resources :users, only: :create
-      resources :sessions, only: :create
+      scope constraints: { format: :json} do
+        resources :users, only: :create
+        resources :sessions, only: :create
+      end
     end
   end
 end
