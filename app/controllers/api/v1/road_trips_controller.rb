66500fb38710a3824dfdd37d3594052068519ff7
Henry Schmid <henry.schmid1@gmail.com>
2021-11-16 16:14:20 -0700

    Add before_action to verify api key

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::RoadTripsController < ApplicationController
+  before_action :verify_api_key, only: :create
+
   def create
     trip = RoadTripFacade.plan_trip(params[:origin], params[:destination])
     render json: RoadTripSerializer.new(trip)
+  end
+
+  private
+
+  def verify_api_key
+    user = User.find_by(api_key: params[:api_key])
+    unauthorized if !user
   end
 end
