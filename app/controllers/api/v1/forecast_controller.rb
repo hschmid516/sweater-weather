fe3517834f0995c6e4a41563c514467250f97fef
Henry Schmid <henry.schmid1@gmail.com>
2021-11-16 14:26:39 -0700

    Add forecast object to weather facade params

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    forecast = WeatherFacade.forecast({ location: params[:location] })
+    forecast = WeatherFacade.forecast(Forecast, { location: params[:location] })
     render json: ForecastSerializer.new(forecast)
   end
 end
